6dcff01b73a8e8dc4a062b91422ec71ae51170cb
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-03 03:28:08 +0000

    docs: add throws

--- src/ok.ts
+++ src/ok.ts
@@ -1,6 +1,9 @@
 import { fromCode } from '@blackglory/http-status'
 export { HTTPError } from '@blackglory/http-status'
 
+/**
+ * @throws {HTTPError}
+ */
 export async function ok(res: Response): Promise<Response> {
   if (!res.ok) {
     const text = await res.text()
